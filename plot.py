100d0c1f6dd33b3d06d4fc843fb9044a257a585d
Sunny Lin <67987511+linlion0311@users.noreply.github.com>
2022-06-09 13:23:57 +0800

    Update and rename test_plot.py to plot.py

--- plot.py
+++ plot.py
@@ -18,12 +18,7 @@ for i in range(len(model2_max_acc)):
     a.append(int(i))
 iter=a
 
-# plt.plot(iter,model2_max_acc,color='r', label='max_acc')
-# plt.legend()
-# plt.show()
-
-
-plt.title("MNIST Classfication YCLin") # title
+plt.title("MNIST Classfication SUNNY") # title
 plt.ylabel("Val_acc") # y label
 plt.xlabel("iteration") # x label
 plt.plot(iter, model1_max_acc,color='r', label='CNN_acc')
@@ -31,43 +26,3 @@ plt.plot(iter,model2_max_acc,color='b', label='FC_acc')
 plt.legend()
 # plt.yticks(np.arange(98,100,step=0.2))
 plt.show()
-
-
-
-
-s=[1000,1500,1600]
-
-# b=[200,400,458]
-
-    # f = open('{}.txt'.format(model))
-    # text = f.read()
-    # text=int(text)
-    # a=[0,1,2,3]
-    # print(text)
-    # f.close()
-
-# s.append(2000)
-# s.append(2000)
-# b.append(600)
-# b.append(600)
-# plt.plot(s,b)
-# print(s)
-# print(b)
-# plt.plot(s, b,color='r', label='max_acc')
-# plt.legend()
-# plt.show()
-
-# path = 'output.txt'
-# f = open(path, 'w')
-# for a in s:
-#     d=float(a)
-#     f.write(f"{d}\n")
-#     print(a)
-# f.close()
-# b=[]
-# f = open('output.txt')
-# for line in f.readlines():
-#     print(line)
-#     b.append(int(line))
-# print(b)
-# f.close
